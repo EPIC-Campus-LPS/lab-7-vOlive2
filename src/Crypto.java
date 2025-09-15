592b4b90824abafcee5f2b1eb4a1d106c0e3c51d
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-15 08:54:08 -0600

    Update Crypto.java

--- src/Crypto.java
+++ src/Crypto.java
@@ -1,3 +1,13 @@
 public class Crypto {
-
+	public static void main(String args[]) {
+		
+	}
+	
+	public String encrypt(String mes, int num) {
+		
+	}
+	
+	public String decrypt(String mes, int num) {
+		
+	}
 }
